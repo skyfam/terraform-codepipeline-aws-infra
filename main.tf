0f704ac0f8a4092bef58aaf95b046b35090f0582
Raza Mehdi <srmk@outlook.com>
2023-12-31 16:13:50 +0500

    Refactor & use CodeBuild source creds.

--- main.tf
+++ main.tf
@@ -2,37 +2,10 @@ data "aws_codestarconnections_connection" "tf-github" {
   name = "TF-Github"
 }
 
-data "aws_s3_bucket" "s3-tf-state" {
-  bucket = "srmk-terraform-state"
+data "aws_secretsmanager_secret" "thirdparty-creds" {
+  name = "thirdparty_access_tokens"
 }
 
-resource "aws_s3_bucket" "s3-bucket-backend" {
-  bucket = var.s3_bucket_name
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_ownership_controls" "s3-bucket-backend-ownership" {
-  bucket = aws_s3_bucket.s3-bucket-backend.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "s3-bucket-backend-acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.s3-bucket-backend-ownership]
-  
-  bucket = aws_s3_bucket.s3-bucket-backend.id
-  acl = "private"
-}
-
-resource "aws_s3_bucket_versioning" "s3-bucket-backend-versioning" {
-  bucket = aws_s3_bucket.s3-bucket-backend.id
-  versioning_configuration {
-    status = "Disabled"
-  }
-}
-
-resource "aws_s3_object" "terraform_folder" {
-  bucket = aws_s3_bucket.s3-bucket-backend.id
-  key    = "terraform_backend/"
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = data.aws_secretsmanager_secret.thirdparty-creds.id
 }
---
+++ s3.tf
@@ -0,0 +1,34 @@
+data "aws_s3_bucket" "s3-tf-state" {
+  bucket = "srmk-terraform-state"
+}
+
+resource "aws_s3_bucket" "s3-bucket-backend" {
+  bucket = var.s3_bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3-bucket-backend-ownership" {
+  bucket = aws_s3_bucket.s3-bucket-backend.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3-bucket-backend-acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3-bucket-backend-ownership]
+  
+  bucket = aws_s3_bucket.s3-bucket-backend.id
+  acl = "private"
+}
+
+resource "aws_s3_bucket_versioning" "s3-bucket-backend-versioning" {
+  bucket = aws_s3_bucket.s3-bucket-backend.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+resource "aws_s3_object" "terraform_folder" {
+  bucket = aws_s3_bucket.s3-bucket-backend.id
+  key    = "terraform_backend/"
+}
