596c6559ee6b45926feb15e54510ab5a6b8cbe98
FarooqShaikh <farooq.shaikh@flashnetworks.com>
2025-05-20 11:38:08 +0530

    random s3 nucket commit

--- s3.tf
+++ s3.tf
@@ -2,20 +2,12 @@ resource "random_id" "bucket_suffix" {
   byte_length = 6
 }
 
-
-
-# data "aws_s3_bucket" "s3-tf-state" {
-#   bucket = "terraform-cicd-aws-pipeline"
-# }
-
-
-
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = data.aws_s3_bucket.s3-tf-state
+  bucket = "example-bucket-${random_id.bucket_suffix.hex}"
 }
 
 output "bucket_name" {
-  value = data.aws_s3_bucket.s3-tf-state
+  value = aws_s3_bucket.example_bucket.bucket
 }
 
 
